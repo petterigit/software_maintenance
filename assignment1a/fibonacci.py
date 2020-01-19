74529fc4152b56b7ad1a1bd943b5355a88beea5e
petterigit <p-makela@outlook.com>
2020-01-19 17:35:05 +0200

    Test runs added

--- assignment1a/fibonacci.py
+++ assignment1a/fibonacci.py
@@ -10,6 +10,7 @@ def fibonacci(*args):
 
     # For command-line usage
 
+
     try:
         n = args[0]
         b = args[1]
@@ -17,23 +18,27 @@ def fibonacci(*args):
         n = 10
         b = -1
 
-    #n = args[0]
-    #b = args[1]
 
+    ### Below if checks commented out for test run 1
     if (isinstance(n, bool)):
         n = 10
     if (isinstance(b, bool)):
         b = -1
 
+
+    ### Below default values changed for test run 2
     try:
         n = int(n)
     except:
         n = 10
+        #n = 1
 
     try:
         b = int(b)
     except:
         b = -1
+        #b = 3
+
 
     file = open("fibonacciResults.txt", "w")
 
